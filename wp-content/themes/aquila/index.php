26a1a1bb8d780b3baaa7defea5c7eda4daf8ecd7
Barry Keith Salmon <salmon.barry1@gmail.com>
2024-04-03 16:10:34 +0800

    Continue with training

--- wp-content/themes/aquila/index.php
+++ wp-content/themes/aquila/index.php
@@ -5,15 +5,12 @@
  * @package Aquila
  */
 
+ get_header();
  ?>
- <!DOCTYPE html>
- <html lang="en">
- <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Wordpress Theme</title>
- </head>
- <body>
-  
- </body>
- </html>
+<div class="content">
+      Content
+</div>
+
+<?php 
+get_footer();
+    
